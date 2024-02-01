bba86c666ae02710366ba767f733b77f1d850e83
Zhu Deng <59046882+zhudeng94@users.noreply.github.com>
2024-02-01 11:28:35 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 12/22/22 10:41 AM
+# @Author  : Zhu Deng
+# @Site    : https://github.com/zhudeng94
+# @File    : main.py
+# @Software: PyCharm
+
+import getAmapIndex
+import datetime
+
+
+def main():
+    # 初始化高德和TomTom的类
+    a = getAmapIndex.Amap()
+
+    # 每天执行一次高德和TomTom的天级数据抓取
+    print('[AMAP] Updating hourly traffic index from Gaode...')
+    a.getAmapData('Hourly')
+    print('[AMAP] Updating daily traffic index from Gaode...')
+    a.getAmapData('Daily')
+
+
+if __name__ == '__main__':
+    main()
